df1a661afabf3ae124e38c226798d5af6ee6b2eb
Kwang Eun Jang <kejang@stanford.edu>
2025-03-11 16:29:15 -0700

    fix importing and setup

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 import setuptools
-import os
-from pathlib import Path
 
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version
 
-module_path = Path(os.path.abspath(__file__)).parent.absolute()
-package_name = "wfista"
-
-try:
-    pkg_version = version(package_name)
-except Exception:
-    pkg_version = "0.0.1"
+pkg_name = "wfista"
+pkg_version = "0.0.1"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name=package_name,
+    name=pkg_name,
     version=pkg_version,
+    packages=setuptools.find_packages(),
+    python_requires=">=3.10",
+    include_package_data=True,
+    package_data={
+        "wfista": [
+            "thresholding/cuda/*.cu",
+        ]
+    },
     author="Kwang Eun Jang",
     author_email="kejang@stanford.edu",
     description="Wavelet-FISTA",
@@ -29,14 +26,5 @@ setuptools.setup(
     url="https://github.com/kejang/wfista",
     project_urls={
         "Bug Tracker": "https://github.com/kejang/wfista/issues",
-    },
-    package_dir={"": "."},
-    packages=setuptools.find_packages(where="."),
-    python_requires=">=3.10",
-    include_package_data=True,
-    package_data={
-        "wfista": [
-            "thresholding/cuda/*.cu",
-        ]
     },
 )
---
+++ wfista/__init__.py
@@ -0,0 +1,5 @@
+from .operators import *
+from .routines import *
+from .utils import *
+from .solver import *
+from .thresholding import *
